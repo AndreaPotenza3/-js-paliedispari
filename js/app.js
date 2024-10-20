29c38c70ad3aec0988904021021d16390f2a385c
AndreaPotenza <andreapoten3@gmail.com>
2024-10-20 21:34:54 +0200

    secondo iniziato

--- js/app.js
+++ js/app.js
@@ -30,3 +30,18 @@ const palindromeWord = paliWord(userWord)
 console.log(palindromeWord)
 
 
+
+
+//Pari e Dispari
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+//Chiediamo un numero all'utente
+const userNumber = prompt('Scegli un numero da 1 a 5')
+console.log(userNumber)
+//Chiediamo un numer al PC
+//const pcNumber = Math.floor(Math.random() * 5) + 1 //numeber 1-5
+//console.log(pcNumber)
+
+function pcNumberGen(number) {
+    
+}
